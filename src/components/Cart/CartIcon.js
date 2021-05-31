8df2500d66e00594d9bd6e41b54c1011c366e8d1
Jithendra K <jithendra14111999@gmail.com>
2021-05-31 16:55:28 +0530

    Header and Dummy Cart
    Header and Dummy Cart added

---
+++ src/components/Cart/CartIcon.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+const CartIcon = () => <ShoppingCartIcon />;
+
+export default CartIcon;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,55 @@
+import React, { Fragment } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, AppBar, Toolbar, Box } from "@material-ui/core";
+import mealsImage from "../../Assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const styles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: "#8a2b06",
+    height: "5rem",
+    boxShadow: "0 2px 8px rgba(0, 0, 0, 0.25)",
+    padding: "0 5%",
+    color: "#ffffff",
+    zIndex: 10,
+  },
+  toolBar: {
+    margin: "auto 0px",
+  },
+  title: {
+    flexGrow: 1,
+  },
+  mainImage: {
+    width: "100%",
+    height: "25rem",
+    zIndex: 0,
+    overflow: "hidden",
+  },
+  img: {
+    width: "110%",
+    height: "100%",
+    objectFit: "cover",
+    transform: "rotateZ(-5deg) translateY(-4rem) translateX(-1rem)",
+  },
+}));
+
+const Header = (props) => {
+  const classes = styles();
+  return (
+    <Fragment>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar className={classes.toolBar}>
+          <Typography variant="h4" component="h1" className={classes.title}>
+            ReactMeals
+          </Typography>
+          <HeaderCartButton />
+        </Toolbar>
+      </AppBar>
+      <Box className={classes.mainImage}>
+        <img className={classes.img} src={mealsImage} alt="A table full of delicious food!" />
+      </Box>
+    </Fragment>
+  );
+};
+
+export default Header;
