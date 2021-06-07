eeabce713f979d1eaacb211c70f83738c391a133
Jithendra K <jithendra14111999@gmail.com>
2021-06-07 12:09:41 +0530

    removed bootstrap.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import("./bootstrap");
+import ReactDOM from "react-dom";
+import React from "react";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
